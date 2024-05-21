099ecce72cd8d4884d37c0e042b594e1fee53f4a
Luis Pedro Lira <luispelirak130@gmail.com>
2024-05-20 18:36:27 -0600

    Archivos Neo4j

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,43 +1,79 @@
+
+/**
+ * 
+ */
 package demo;
 
-import java.util.Map;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.LinkedList;
+import java.util.Scanner;
 
-import org.neo4j.driver.AuthTokens;
-import org.neo4j.driver.GraphDatabase;
-import org.neo4j.driver.Record;
-import org.neo4j.driver.QueryConfig;
-import org.neo4j.driver.RoutingControl;
 
+/**
+ * @author MAAG
+ *
+ */
 public class App {
 
-    private static final String URI = "<neo4j+s://42ccd2dc.databases.neo4j.io>";
-    private static final String USER = "<neo4j>";
-    private static final String PASSWORD = "<Lc47HWTZO6HLPKWAftFPhNetzhYBfH_AbjtNv7gY-ew>";
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String username = "neo4j";
+		String password = "densities-feelings-tubes";
+		String boltURL = "bolt://44.192.99.82:7687";
+		
+		try ( EmbeddedNeo4j db = new EmbeddedNeo4j( boltURL, username, password ) )
+        {
+		 	LinkedList<String> myactors = db.getActors();
+		 	
+		 	for (int i = 0; i < myactors.size(); i++) {
+		 		System.out.println(myactors.get(i));
+		 	}
+        	
+        } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Ingrese el nombre de un actor");
+		Scanner in = new Scanner(System.in);
+		String myActor = in.nextLine();
+		
+		try ( EmbeddedNeo4j db = new EmbeddedNeo4j( boltURL, username, password ) )
+        {
+		 	LinkedList<String> movies = db.getMoviesByActor(myActor);
+		 	
+		 	for (int i = 0; i < movies.size(); i++) {
+		 		System.out.println(movies.get(i));
+		 		
+		 	}
+        	
+        } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Ingrese una nueva Pelicula");
+		System.out.println("Ingrese Nombre de la pelicula");
+		String movieTitle = in.nextLine();
+		System.out.println("Ingrese Año Lanzamiento");
+		int movieReleaseYear = Integer.parseInt(in.nextLine());
+		System.out.println("Ingrese Descripcion");
+		String movieTagLine = in.nextLine();
+		
+		try ( EmbeddedNeo4j db = new EmbeddedNeo4j( boltURL, username, password ) )
+        {
+		 	String result = db.insertMovie(movieTitle, movieReleaseYear, movieTagLine);
+		 	
+		 	if (result.equalsIgnoreCase("OK")) {
+		 		System.out.println("Pelicula insertada correctamente");
+		 	}
+        	
+        } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    public static void main(String... args) {
-
-        try (var driver = GraphDatabase.driver(URI, AuthTokens.basic(USER, PASSWORD))) {
-
-            
-
-            try {
-                //CREACIÓN DE NODOS
-                people.forEach(person -> {
-                    var result = driver.executableQuery("CREATE (p:Person {name: $Jett, role: Duelista})")
-                    var result = driver.executableQuery("CREATE (p:Person {name: $Breach, role: Iniciador})")
-                    var result = driver.executableQuery("CREATE (p:Person {name: $Viper, role: Controlador})")
-                    var result = driver.executableQuery("CREATE (p:Person {name: $Clove, role: Controlador})")
-                        .withConfig(QueryConfig.builder().withDatabase("neo4j").build())
-                        .withParameters(Map.of("person", person))
-                        .execute();
-                });
-
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                System.exit(1);
-            }
-        }
-    }
 }
